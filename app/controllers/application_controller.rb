38a65514f35669579e689b830062d8b3cec60ba6
Edward Fernando <edward.fer@gmail.com>
2014-10-28 23:04:49 +0700

    initialize @controller to access allegro api globally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  	before_filter :initialize_client
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def initialize_client
+    @consumer = Allegro::Client.new
+  end
 end
